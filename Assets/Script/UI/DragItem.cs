147cc67118664c11305a66f74300c404eff480d2
cju4409 <cj4409@naver.com>
2024-10-29 11:48:44 +0900

    추가: 인벤토리 구현 & 드래그 앤 드롭기능

---
+++ Assets/Script/UI/DragItem.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//IBeginDragHandler : 드래그 시작,IDragHandler : 드래그 중, IEndDragHandler : 드래그 끝
+public class DragItem : ImageProperty, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Vector2 dragOffset = Vector3.zero;
+    public Transform myParent
+    {
+        get; private set;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        myParent = transform.parent;
+        transform.SetParent(transform.parent.parent.parent);
+        dragOffset = (Vector2)transform.position - eventData.position;
+        myImage.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position + dragOffset;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(myParent);
+        myImage.raycastTarget = true;
+        transform.localPosition = Vector3.zero;
+    }
+
+
+    public void OnChangeParent(Transform p, bool change = false)
+    {
+        if (change)
+        {
+            myParent = p;
+            transform.SetParent(myParent);
+            transform.localPosition = Vector3.zero;
+            myParent.GetComponent<DropItem>().OnChangeChild(this);
+        }
+        else
+        {
+            myParent.GetComponent<DropItem>().OnChangeChild(null);
+            myParent = p;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Script/UI/DropItem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// IDropHandler: 드래그하고나서 드롭했을때 실행
+public class DropItem : MonoBehaviour, IDropHandler
+{
+    DragItem myChild = null;
+    public void OnDrop(PointerEventData eventData)
+    {
+        // eventData.pointerDrag : 드랍한 곳에 있는 오브젝트
+        DragItem newChild = eventData.pointerDrag.GetComponent<DragItem>();
+        if (newChild == null) return;
+        if (myChild != null)
+        {
+            myChild.OnChangeParent(newChild.myParent, true);
+        }
+        newChild.OnChangeParent(transform);
+        OnChangeChild(newChild);
+    }
+
+    public void OnChangeChild(DragItem child)
+    {
+        myChild = child;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myChild = GetComponentInChildren<DragItem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
